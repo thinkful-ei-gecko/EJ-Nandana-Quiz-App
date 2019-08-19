38234279f8f6405771fbcf95c1ef439e86418413
nrrao <nandanarao@yahoo.com>
2019-08-19 18:17:50 -0400

    random generator

--- main.js
+++ main.js
@@ -4,17 +4,33 @@
 let interval = 0;
 let score = 0;
 
+
+
+function randomNumbergenerator(){
+  let questionArray=[];
+  for (let i=1;questionArray.length<5;i++){
+    let randomNumber=Math.floor(Math.random()*10);
+    if(questionArray.indexOf(randomNumber) === -1){
+      questionArray.push(randomNumber);
+    }
+  
+  }
+  return questionArray;
+}
+
 function loadQuestion() {
-    console.log('loadQuestion');
-    $('main').append(`<div class="questionBlock">
-    <img class="questionImg" src="${STORE[interval].img}">
+  console.log('loadQuestion');
+  console.log("inside load function"+interval);
+  config.questionNumber=config.questionNumberArray[interval];
+  $('main').append(`<div class="questionBlock">
+    <img class="questionImg" src="${STORE[config.questionNumber].img}">
     
     <form>
         <fieldset>
-            <label class="answerBlock one"><input type="radio" value='${STORE[interval].questionOptions[0]}' name="answer" required><span>${STORE[interval].questionOptions[0]}</span></label>
-            <label class="answerBlock two"><input type="radio" value='${STORE[interval].questionOptions[1]}' name="answer" required><span>${STORE[interval].questionOptions[1]}</span></label>
-            <label class="answerBlock three"><input type="radio" value='${STORE[interval].questionOptions[2]}' name="answer" required><span>${STORE[interval].questionOptions[2]}</span></label>
-            <label class="answerBlock four"><input type="radio" value='${STORE[interval].questionOptions[3]}' name="answer" required><span>${STORE[interval].questionOptions[3]}</span></label>
+            <label class="answerBlock one"><input type="radio" value='${STORE[config.questionNumber].questionOptions[0]}' name="answer" required><span>${STORE[config.questionNumber].questionOptions[0]}</span></label>
+            <label class="answerBlock two"><input type="radio" value='${STORE[config.questionNumber].questionOptions[1]}' name="answer" required><span>${STORE[config.questionNumber].questionOptions[1]}</span></label>
+            <label class="answerBlock three"><input type="radio" value='${STORE[config.questionNumber].questionOptions[2]}' name="answer" required><span>${STORE[config.questionNumber].questionOptions[2]}</span></label>
+            <label class="answerBlock four"><input type="radio" value='${STORE[config.questionNumber].questionOptions[3]}' name="answer" required><span>${STORE[config.questionNumber].questionOptions[3]}</span></label>
             <button type="submit" class="submitButton">Submit</button>
         </fieldset>
     </form>
@@ -23,19 +39,20 @@ function loadQuestion() {
 
 }
 
+
 function rightAnswer() {
-    let correctAnswer = `${STORE[interval].correntAnswer}`
-    updateScore();
-    $('header').html(`
+  let correctAnswer = `${STORE[config.questionNumber].correntAnswer}`;
+  updateScore();
+  $('header').html(`
         <div class="topBanner">
           <p class="questionRightInterval">${score}/5</p>
           <img class="bannerIcon" src="./images/engine.png" alt="starting engine">
         </div>
         <div>
         <p class="questionInterval">Question: ${interval + 1}/5</p>
-        </div>`)
+        </div>`);
 
-    $('main').html(`<div class="rightAnswer">
+  $('main').html(`<div class="rightAnswer">
     <img class="rightImg" src="./images/like.png">
     
     <h2 class="">YOU GOT IT RIGHT!</h2>
@@ -43,20 +60,20 @@ function rightAnswer() {
     
     </div>`);
 
-    generateNewQuestion();
+  generateNewQuestion();
 }
 
 function wrongAnswer() {
-    let correctAnswer = `${STORE[interval].correntAnswer}`;
-    $('header').html(`
+  let correctAnswer = `${STORE[config.questionNumber].correntAnswer}`;
+  $('header').html(`
         <div class="topBanner">
           <p class="questionRightInterval">${score}/5</p>
           <img class="bannerIcon" src="./images/engine.png" alt="starting engine">
         </div>
         <div>
         <p class="questionInterval">Question: ${interval + 1}/5</p>
-        </div>`)
-    $('main').html(`<div class="wrongAnswer">
+        </div>`);
+  $('main').html(`<div class="wrongAnswer">
     <img class="wrongImg" src="./images/tools.png" alt="tools making a X sign">
     
     <h2 class="">YOU GOT IT WRONG!</h2>
@@ -65,23 +82,23 @@ function wrongAnswer() {
     <button class="nextQuestion">Next Question</button>
     
     </div>`);
-    console.log(interval);
-    console.log(score);
-    generateNewQuestion();
+  console.log(interval);
+  console.log(score);
+  generateNewQuestion();
 }
 
 function countInterval() {
-    interval++;
+  interval++;
 }
 
 function updateScore() {
-    score++;
+  score++;
 }
 
 function generateNewQuestion() {
-    console.log('inside of generatequestion')
-    event.preventDefault();
-    $('header').html(`
+  console.log('inside of generatequestion');
+  event.preventDefault();
+  $('header').html(`
     <div class="topBanner">
         <p class="questionRightInterval">${score}/5</p>
         <img class="bannerIcon" src="./images/engine.png" alt="starting engine">
@@ -90,7 +107,7 @@ function generateNewQuestion() {
     <p class="questionInterval">Question: ${interval + 1}/5</p>
     </div>`);
 
-    loadQuestion();
+  loadQuestion();
     
 }
 
@@ -100,14 +117,14 @@ function generateNewQuestion() {
 //
 
 function clear(){
-    score = 0;
-    interval = 0;
+  score = 0;
+  interval = 0;
 }
 
 function startQuiz() {
-        $('main').empty();
-        $('header').empty();
-        $('header').html(`
+  $('main').empty();
+  $('header').empty();
+  $('header').html(`
         <div class="topBanner">
           <p class="questionRightInterval">${score}/5</p>
           <img class="bannerIcon" src="./images/engine.png" alt="starting engine">
@@ -115,14 +132,17 @@ function startQuiz() {
         <div>
         <p class="questionInterval">Question: ${interval + 1}/5</p>
         </div>`);
-        loadQuestion();
+  config.questionNumberArray=randomNumbergenerator();
+  console.log(config.questionNumberArray);
+  loadQuestion();
 }
 
+
 function startQuizScreen() {
-    console.log("startQuizScreen");
-    $('header').addClass('initial');
-    $('header').html(`<img class="icon" src="./images/engine.png" alt="starting engine">`);
-    $('main').html(`
+  console.log('startQuizScreen');
+  $('header').addClass('initial');
+  $('header').html('<img class="icon" src="./images/engine.png" alt="starting engine">');
+  $('main').html(`
     <div class="block1">
         <h1>Start Your Engine</h1>
         <button class='beginQuiz'>Begin Quiz</button>
@@ -131,69 +151,71 @@ function startQuizScreen() {
 }
 
 function allHandlers(){
-    $('main').on('click', '.beginQuiz', function(){
-        startQuiz();
-        $('.initial').removeClass();
-    });
+  $('main').on('click', '.beginQuiz', function(){
+    
+    startQuiz();
+    $('.initial').removeClass();
+  });
 
-    $('main').on('click', '.nextQuestion', function (event) {
-        generateNewQuestion();
-    });
-
-
-    $('main').on('click', '.restartButton', function(){
-        console.log("restart button clicked");
-        startQuizScreen();
-        clear();
-    });
+  $('main').on('click', '.nextQuestion', function (event) {
+    generateNewQuestion();
+  });
 
 
-    $('main').submit(function (event) {
-        event.preventDefault();
-        let selectedAnswer = $('input:checked');
-        let answer = selectedAnswer.val();
-        let correctAnswer = `${STORE[interval].correntAnswer}`;
-
-        if (correctAnswer === answer) {
-            console.log('You are right!');
-            rightAnswer();
-        } else {
-            console.log('Wrong! The correct answer is: ' + correctAnswer);
-            wrongAnswer();
-        }
-        countInterval();
-        $('.questionBlock').remove();
+  $('main').on('click', '.restartButton', function(){
+    console.log('restart button clicked');
+    startQuizScreen();
+    clear();
+  });
 
 
-        console.log(selectedAnswer);
-        console.log(answer);
-        console.log(correctAnswer);
+  $('main').submit(function (event) {
+    event.preventDefault();
+    let selectedAnswer = $('input:checked');
+    let answer = selectedAnswer.val();
+    let correctAnswer = `${STORE[config.questionNumber].correntAnswer}`;
 
-    });
+    if (correctAnswer === answer) {
+      console.log('You are right!');
+      rightAnswer();
+    } else {
+      console.log('Wrong! The correct answer is: ' + correctAnswer);
+      wrongAnswer();
+    }
+    countInterval();
+    $('.questionBlock').remove();
 
-    $('main').on('click', '.nextQuestion', function(){
-        if (interval < 5) {
-            $('main').html(`<div class="questionBlock">
-            <img class="questionImg" src="${STORE[interval].img}">
+
+    console.log(selectedAnswer);
+    console.log(answer);
+    console.log(correctAnswer);
+
+  });
+
+  $('main').on('click', '.nextQuestion', function(){
+    if (interval < 5) {
+        console.log("inside"+ interval);
+      $('main').html(`<div class="questionBlock">
+            <img class="questionImg" src="${STORE[config.questionNumber].img}">
             
             <form>
                 <fieldset>
-                    <label class="answerBlock one"><input type="radio" value='${STORE[interval].questionOptions[0]}' name="answer" required><span>${STORE[interval].questionOptions[0]}</span></label>
-                    <label class="answerBlock two"><input type="radio" value='${STORE[interval].questionOptions[1]}' name="answer" required><span>${STORE[interval].questionOptions[1]}</span></label>
-                    <label class="answerBlock three"><input type="radio" value='${STORE[interval].questionOptions[2]}' name="answer" required><span>${STORE[interval].questionOptions[2]}</span></label>
-                    <label class="answerBlock four"><input type="radio" value='${STORE[interval].questionOptions[3]}' name="answer" required><span>${STORE[interval].questionOptions[3]}</span></label>
+                    <label class="answerBlock one"><input type="radio" value='${STORE[config.questionNumber].questionOptions[0]}' name="answer" required><span>${STORE[config.questionNumber].questionOptions[0]}</span></label>
+                    <label class="answerBlock two"><input type="radio" value='${STORE[config.questionNumber].questionOptions[1]}' name="answer" required><span>${STORE[config.questionNumber].questionOptions[1]}</span></label>
+                    <label class="answerBlock three"><input type="radio" value='${STORE[config.questionNumber].questionOptions[2]}' name="answer" required><span>${STORE[config.questionNumber].questionOptions[2]}</span></label>
+                    <label class="answerBlock four"><input type="radio" value='${STORE[config.questionNumber].questionOptions[3]}' name="answer" required><span>${STORE[config.questionNumber].questionOptions[3]}</span></label>
                     <button type="submit" class="submitButton">Submit</button>
                 </fieldset>
             </form>
             
             </div>`);
-        } else {
+    } else {
     
-            console.log("quiz is over");
+      console.log('quiz is over');
     
-            if (score < 3){
-                $('header').find('.questionInterval').remove();
-                $('main').html(`
+      if (score < 3){
+        $('header').find('.questionInterval').remove();
+        $('main').html(`
             <div class="loserBlock">
                 <img class="loserIcon" src="./images/handbrake.png">
             
@@ -203,9 +225,9 @@ function allHandlers(){
             
             </div>`);
         
-        } else {    
-            $('header').find('.questionInterval').remove();
-            $('main').html(`
+      } else {    
+        $('header').find('.questionInterval').remove();
+        $('main').html(`
             <div class="winnerBlock">
                 <img class="winnerIcon" src="./images/car-key.png">
             
@@ -214,9 +236,9 @@ function allHandlers(){
             
             </div>`);
     
-            }
+      }
     }
-});
+  });
 
 
 }
--- questions.js
+++ questions.js
@@ -1,55 +1,65 @@
-const STORE = [{
-    question: 'Lamborghini',
-    questionOptions: ['Japan', 'Italy', 'United States', 'Switzerland'],
-    img: 'https://cdn.pixabay.com/photo/2015/12/22/22/04/lamborghini-1104868_1280.jpg',
-    correntAnswer: 'Italy'
-}, {
-    question: 'Porshe',
-    questionOptions: ['Germany', 'Denmark', 'China', 'Russia'],
-    img: 'https://cdn.pixabay.com/photo/2014/05/21/15/38/coat-of-arms-349889_1280.jpg',
-    correntAnswer: 'Germany'
-}, {
-    question: 'McLaren',
-    questionOptions: ['Japan', 'Italy', 'United Kingdom', 'United States'],
-    img: 'https://cdn.pixabay.com/photo/2017/03/11/21/36/iaa-2135916_1280.jpg',
-    correntAnswer: 'United Kingdom'
-}, {
-    question: 'Bentley',
-    questionOptions: ['Spain', 'Sweden', 'United States', 'United Kingdom'],
-    img: 'https://images.unsplash.com/photo-1519572422788-07033d956866?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-    correntAnswer: 'United Kingdom'
-}, {
-    question: 'Rolls Royce',
-    questionOptions: ['United Kingdom', 'Germany', 'United States', 'Denmark'],
-    img: 'https://images.unsplash.com/photo-1521405617584-1d9867aecad3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-    correntAnswer: 'United Kingdom'
-}, {
-    question: 'Mercedes Benz',
-    questionOptions: ['Japan', 'United Kingdom', 'Germany', 'United States'],
-    img: 'https://images.unsplash.com/photo-1532578498858-e21a39e0a449?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-    correntAnswer: 'Germany'
-}, {
-    question: 'Ford',
-    questionOptions: ['Germany', 'Spain', 'United States', 'Italy'],
-    img: 'https://images.unsplash.com/photo-1530936541512-05ec5b6c6e71?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-    correntAnswer: 'United States'
-}, {
-    question: 'BMW',
-    questionOptions: ['Germany', 'Italy', 'Denmark', 'United States'],
-    img: 'https://images.unsplash.com/photo-1552234816-0ea7b995a55c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1001&q=80',
-    correntAnswer: 'Germany'
-}, {
-    question: 'Cadillac',
-    questionOptions: ['Germany', 'Japan', 'United States', 'Denmark'],
-    img: 'https://images.unsplash.com/photo-1457088846009-9dbcf9d7643d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-    correntAnswer: 'United States'
-}, {
-    question: 'Tesla',
-    questionOptions: ['Japan', 'United States', 'Spain', 'United Kingdom'],
-    img: 'https://images.unsplash.com/photo-1518194951576-93eec1215e9a?ixlib=rb-1.2.1&auto=format&fit=crop&w=1399&q=80',
-    correntAnswer: 'United States'
-}]
+'use strict';
+/*eslint-env jquery*/
 
+const STORE = [{
+  question: 'Lamborghini',
+  questionOptions: ['Japan', 'Italy', 'United States', 'Switzerland'],
+  img: 'https://cdn.pixabay.com/photo/2015/12/22/22/04/lamborghini-1104868_1280.jpg',
+  correntAnswer: 'Italy'
+}, {
+  question: 'Porshe',
+  questionOptions: ['Germany', 'Denmark', 'China', 'Russia'],
+  img: 'https://cdn.pixabay.com/photo/2014/05/21/15/38/coat-of-arms-349889_1280.jpg',
+  correntAnswer: 'Germany'
+}, {
+  question: 'McLaren',
+  questionOptions: ['Japan', 'Italy', 'United Kingdom', 'United States'],
+  img: 'https://cdn.pixabay.com/photo/2017/03/11/21/36/iaa-2135916_1280.jpg',
+  correntAnswer: 'United Kingdom'
+}, {
+  question: 'Bentley',
+  questionOptions: ['Spain', 'Sweden', 'United States', 'United Kingdom'],
+  img: 'https://images.unsplash.com/photo-1519572422788-07033d956866?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+  correntAnswer: 'United Kingdom'
+}, {
+  question: 'Rolls Royce',
+  questionOptions: ['United Kingdom', 'Germany', 'United States', 'Denmark'],
+  img: 'https://images.unsplash.com/photo-1521405617584-1d9867aecad3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+  correntAnswer: 'United Kingdom'
+}, {
+  question: 'Mercedes Benz',
+  questionOptions: ['Japan', 'United Kingdom', 'Germany', 'United States'],
+  img: 'https://images.unsplash.com/photo-1532578498858-e21a39e0a449?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+  correntAnswer: 'Germany'
+}, {
+  question: 'Ford',
+  questionOptions: ['Germany', 'Spain', 'United States', 'Italy'],
+  img: 'https://images.unsplash.com/photo-1530936541512-05ec5b6c6e71?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+  correntAnswer: 'United States'
+}, {
+  question: 'BMW',
+  questionOptions: ['Germany', 'Italy', 'Denmark', 'United States'],
+  img: 'https://images.unsplash.com/photo-1552234816-0ea7b995a55c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1001&q=80',
+  correntAnswer: 'Germany'
+}, {
+  question: 'Cadillac',
+  questionOptions: ['Germany', 'Japan', 'United States', 'Denmark'],
+  img: 'https://images.unsplash.com/photo-1457088846009-9dbcf9d7643d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+  correntAnswer: 'United States'
+}, {
+  question: 'Tesla',
+  questionOptions: ['Japan', 'United States', 'Spain', 'United Kingdom'],
+  img: 'https://images.unsplash.com/photo-1518194951576-93eec1215e9a?ixlib=rb-1.2.1&auto=format&fit=crop&w=1399&q=80',
+  correntAnswer: 'United States'
+}];
+
+
+const config={
+  questionNumberArray:[],//global
+  questionNumber:0,
+
+};
+  
 
 //make a form that takes a button sumbit 
 //when the button is clicked it will change colors
