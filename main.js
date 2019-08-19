d24564705a1a4cda49b3d533ba6c903ad334fbff
nrrao <nandanarao@yahoo.com>
2019-08-18 22:15:06 -0400

    nrr branch with all working files

--- main.js
+++ main.js
@@ -1,172 +1,158 @@
 'use strict';
 /*eslint-env jquery*/
 
-let interval = 0;
-let score = 0;
 
-// function randomNumbergenerator(){
-//     let questionArray=[1];
-//     for (let i=1;questionArray.length<5;i++){
-//       let randomNumber=Math.floor(Math.random()*10);
-//       if(questionArray.indexOf(randomNumber) === -1){
-//         questionArray.push(randomNumber);
-//       }
+let globalQuestionNumbersArray = null;
+let globalScore = 0;
+let globalQuestionNumbersIndex = -1;
+let globalQuestionNumber = -1;
 
-//     }
-//     return questionArray;
-//    }
+function randomNumbergenerator(){
+  let questionArray=[];
+  for (let i=1;questionArray.length<5;i++){
+    let randomNumber=Math.floor(Math.random()*10);
+    if(questionArray.indexOf(randomNumber) === -1){
+      questionArray.push(randomNumber);
+    }
 
-// let car = randomNumbergenerator();
-
-
-// console.log(car);
+  }
+  return questionArray;
+}
 
 function loadQuestion() {
-    $('main').append(`<div class="questionBlock">
-    <img class="questionImg" src="${STORE[interval].img}">
+  incrementQuestionNumber();
+  globalQuestionNumber=globalQuestionNumbersArray[globalQuestionNumbersIndex];
+  
+  $('.scores').show();
+  $('header').find('#q1').html(`${globalQuestionNumbersIndex+1}/${globalQuestionNumbersArray.length}`);
+  $('main').append(` 
+
+  <div class="questionBlock">
+    <h2>${STORE[globalQuestionNumber].question}</h2>
     
-    <form>
+    <img class="questionImg" src="${STORE[globalQuestionNumber].img}">
+    <form class="questionForm">
         <fieldset>
-            <label class="answerBlock one"><input type="radio" value='${STORE[interval].questionOptions[0]}' name="answer" required><span>${STORE[interval].questionOptions[0]}</span></label>
-            <label class="answerBlock two"><input type="radio" value='${STORE[interval].questionOptions[1]}' name="answer" required><span>${STORE[interval].questionOptions[1]}</span></label>
-            <label class="answerBlock three"><input type="radio" value='${STORE[interval].questionOptions[2]}' name="answer" required><span>${STORE[interval].questionOptions[2]}</span></label>
-            <label class="answerBlock four"><input type="radio" value='${STORE[interval].questionOptions[3]}' name="answer" required><span>${STORE[interval].questionOptions[3]}</span></label>
+            <label class="answerBlock one"><input type="radio" value='${STORE[globalQuestionNumber].questionOptions[0]}' name="answer" required>${STORE[globalQuestionNumber].questionOptions[0]}</label>
+            <label class="answerBlock two"><input type="radio" value='${STORE[globalQuestionNumber].questionOptions[1]}' name="answer" required>${STORE[globalQuestionNumber].questionOptions[1]}</label>
+            <label class="answerBlock three"><input type="radio" value='${STORE[globalQuestionNumber].questionOptions[2]}' name="answer" required>${STORE[globalQuestionNumber].questionOptions[2]}</label>
+            <label class="answerBlock four"><input type="radio" value='${STORE[globalQuestionNumber].questionOptions[3]}' name="answer" required>${STORE[globalQuestionNumber].questionOptions[3]}</label>
             <button type="submit" class="submitButton">Submit</button>
         </fieldset>
     </form>
     
     </div>`);
 
+
 }
 
-function checkQuestion() {
-    $('main').submit(function (event) {
-        event.preventDefault();
-        let selectedAnswer = $('input:checked');
-        let answer = selectedAnswer.val();
-        let correctAnswer = `${STORE[interval].correntAnswer}`;
-
-        if (correctAnswer === answer) {
-            console.log('You are right!');
-            rightAnswer();
-        } else {
-            console.log('Wrong! The correct answer is: ' + correctAnswer);
-            wrongAnswer();
-        }
-        countInterval();
-        $('.questionBlock').remove();
-
-
-        console.log(selectedAnswer);
-        console.log(answer);
-        console.log(correctAnswer);
-
-    });
-}
-
-function rightAnswer() {
-    let correctAnswer = `${STORE[interval].correntAnswer}`
-    updateScore();
-    $('header').html(`
-    <div class="topBanner">
-        <p>${score}/10</p>
-    </div>`)
-
-    $('main').append(`<div class="questionBlockRight">
-    <img class="ansImg" src="./images/like.png">
+function registerCallbackCheckAnswer() {
+  $('main').submit(function (event) {
+    event.preventDefault();
+    let selectedAnswer = $('input:checked');
+    let answer = selectedAnswer.val();
     
-    <h2>YOU GOT IT RIGHT!</h2>
+    let rightImage = './images/like.png';
+    let wrongImage = './images/tools.png';
+    let userMessageRight = 'YOU GOT IT RIGHT!';
+    let userMessageWrong = 'YOU GOT IT WRONG!!';
 
-    <button class="nextQuestion">Next Question</button>
-    
-    </div>`);
+    let selectedImage = null;
+    let userMessage = '';
 
-    console.log(score);
-    generateNewQuestion();
-}
+    let correctAnswer = `${STORE[globalQuestionNumber].correntAnswer}`;
 
-function wrongAnswer() {
-    let correctAnswer = `${STORE[interval].correntAnswer}`;
-    $('main').append(`<div class="questionBlockWrong">
-    <img class="ansImg" src="./images/tools.png">
-    
-    <h2>YOU GOT IT WRONG!</h2>
-    <p>The correct answer was ${correctAnswer}!</p>
+    if (correctAnswer === answer) {
+      selectedImage = rightImage; 
+      userMessage = userMessageRight;
+      incrementScore();
 
-    <button class="nextQuestion">Next Question</button>
-    
-    </div>`);
-    console.log()
-    generateNewQuestion();
-}
-
-function countInterval() {
-    interval++;
-}
-
-function updateScore() {
-    score++;
-}
-
-function generateNewQuestion() {
-    $('main').on('click', '.nextQuestion', function (event) {
-        event.preventDefault();
-        $('main').find('div').remove();
-        if (interval < 5){$('main').append(`<div class="questionBlock">
-        <img class="questionImg" src="${STORE[interval].img}">
-        
-        <form>
-            <fieldset>
-                <label class="answerBlock one"><input type="radio" value='${STORE[interval].questionOptions[0]}' name="answer" required><span>${STORE[interval].questionOptions[0]}</span></label>
-                <label class="answerBlock two"><input type="radio" value='${STORE[interval].questionOptions[1]}' name="answer" required><span>${STORE[interval].questionOptions[1]}</span></label>
-                <label class="answerBlock three"><input type="radio" value='${STORE[interval].questionOptions[2]}' name="answer" required><span>${STORE[interval].questionOptions[2]}</span></label>
-                <label class="answerBlock four"><input type="radio" value='${STORE[interval].questionOptions[3]}' name="answer" required><span>${STORE[interval].questionOptions[3]}</span></label>
-                <button type="submit" class="submitButton">Submit</button>
-            </fieldset>
-        </form>
-        
-        </div>`);
     } else {
-        $('main').on('click', '.restartButton', function (event) {
-            location.reload();
-          });
-        if (score < 3){
-            $('main').append(`
-        <div class="questionBlock">
-            <img class="questionImg" src="./images/handbrake.png">
-        
-            <h2>Thanks for trying but it looks like you need to check your 
-            engine and try again!</h2>
-            <button type="button" class="restartButton">Take the quiz again</button>
-        
-        </div>`);} else {
-        
-        $('main').append(`
-        <div class="questionBlock">
-            <img class="questionImg" src="./images/car-key.png">
-        
-            <h2>Great Job! Looks like you really are a car junkie!</h2>
-            <button type="button" class="restartButton">Take the quiz again</button>
+      
+      selectedImage = wrongImage; 
+      userMessage = userMessageWrong;
+      
+    }
+    $('header').find('#s1').html(`${globalScore}`);
+    $('main').find('.questionBlock').remove();
+    $('main').append(`<div class="checkAnswer">
+    <img class="ansImg" src=${selectedImage}>
+    <h2>${userMessage}</h2>
+    <button class="buttonClass">Next Question</button>
+    </div>`);
+  });
+}
+
+
+function incrementQuestionNumber() {
+  globalQuestionNumbersIndex++;
+}
+
+function incrementScore() {
+  globalScore++;
+}
+
+function registerCallbackNextQuestion() {
+  $('main').on('click', '.buttonClass', function (event) {
+    event.preventDefault();
+    $('main').find('div').remove();
+    
+    if (globalQuestionNumbersIndex < globalQuestionNumbersArray.length-1){
+      loadQuestion();
+    }  
+    else if(globalQuestionNumbersIndex === globalQuestionNumbersArray.length-1) {
+      $('main').find('div').remove();
+      restarQuiz();
+    }
+  });
+}
+    
+function restarQuiz(){
+  if (globalScore < 3){
+    $('main').append(`<div class="checkAnswer">
+            <img class="ansImg" src="./images/handbrake.png">
+            <h2>Your Score is ${globalScore}</h2>
+            <h3>Thanks for trying but it looks like you need to check your 
+            engine and try again!</h3>
+            <button type="button" class="buttonClass">Take the quiz again</button>
         
         </div>`);
-
-        }
-    }
-    })
+    $('main').on('click', '.buttonClass', function (event) {
+      location.reload();
+    });
+  } else {
+        
+    $('main').append(`<div class="checkAnswer">
+            <img class="ansImg" src="./images/car-key.png">
+            <h2>Your Score is ${globalScore}</h2>
+            <h3>Great Job! Looks like you really are a car junkie!</h3>
+            <button type="button" class="buttonClass">Take the quiz again</button>
+        </div>`);
+    $('main').on('click', '.buttonClass', function (event) {
+      location.reload();
+    });
+  }
 }
+  
 
 function startQuiz() {
-    $('main').on('click', '.beginQuiz', function (event) {
-        event.preventDefault();
-        $('main').find('.myForm').remove();
-        $('header').find('img').remove();
-        loadQuestion();
-        $('header').append(`
-        <div class="topBanner">
-          <p>${score}/10</p>
-        </div>`)
-    });
+
+  $('main').on('click', '.beginQuiz', function (event) {
+    event.preventDefault();
+    $('section').removeClass('heading');
+    $('main').find('.myForm').remove();
+    $('main').find('img').remove();
+    globalQuestionNumbersArray = randomNumbergenerator();
+    console.log(globalQuestionNumbersArray);
+    
+    loadQuestion();
+  });
 }
 
-$(startQuiz);
-$(checkQuestion);
+function createQuiz(){
+startQuiz();
+registerCallbackCheckAnswer();
+registerCallbackNextQuestion();
+}
+
+$(createQuiz);
--- questions.js
+++ questions.js
@@ -2,55 +2,55 @@
 /// in nrr branch
 
 const STORE = [{
-    question: 'Lamborghini',
-    questionOptions: ['Japan', 'Italy', 'United States', 'Switzerland'],
-    img: 'https://images.unsplash.com/photo-1517779894653-4c49f68b4d66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2234&q=80',
-    correntAnswer: 'Japan'
+  question: 'Lamborghini',
+  questionOptions: ['Japan', 'Italy', 'United States', 'Switzerland'],
+  img: 'https://images.unsplash.com/photo-1517779894653-4c49f68b4d66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2234&q=80',
+  correntAnswer: 'Italy'
 }, {
-    question: 'Porshe',
-    questionOptions: ['Germany', 'Denmark', 'China', 'Russia'],
-    img: 'https://images.unsplash.com/photo-1564694457220-0efef1a70d6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1001&q=80',
-    correntAnswer: 'Germany'
+  question: 'Porshe',
+  questionOptions: ['Germany', 'Denmark', 'China', 'Russia'],
+  img: 'https://images.unsplash.com/photo-1564694457220-0efef1a70d6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1001&q=80',
+  correntAnswer: 'Germany'
 }, {
-    question: 'McLaren',
-    questionOptions: ['Japan', 'Italy', 'United Kingdom', 'United States'],
-    img: 'https://cdn.pixabay.com/photo/2017/03/11/21/36/iaa-2135916_1280.jpg',
-    correntAnswer: 'United Kingdom'
+  question: 'McLaren',
+  questionOptions: ['Japan', 'Italy', 'United Kingdom', 'United States'],
+  img: 'https://cdn.pixabay.com/photo/2017/03/11/21/36/iaa-2135916_1280.jpg',
+  correntAnswer: 'United Kingdom'
 }, {
-    question: 'Bentley',
-    questionOptions: ['Spain', 'Sweden', 'United States', 'United Kingdom'],
-    img: 'https://images.unsplash.com/photo-1519572422788-07033d956866?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-    correntAnswer: 'United Kingdom'
+  question: 'Bentley',
+  questionOptions: ['Spain', 'Sweden', 'United States', 'United Kingdom'],
+  img: 'https://images.unsplash.com/photo-1519572422788-07033d956866?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+  correntAnswer: 'United Kingdom'
 }, {
-    question: 'Rolls Royce',
-    questionOptions: ['United Kingdom', 'Germany', 'United States', 'Denmark'],
-    img: 'https://images.unsplash.com/photo-1521405617584-1d9867aecad3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-    correntAnswer: 'United Kingdom'
+  question: 'Rolls Royce',
+  questionOptions: ['United Kingdom', 'Germany', 'United States', 'Denmark'],
+  img: 'https://images.unsplash.com/photo-1521405617584-1d9867aecad3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+  correntAnswer: 'United Kingdom'
 }, {
-    question: 'Mercedes Benz',
-    questionOptions: ['Japan', 'United Kingdom', 'Germany', 'United States'],
-    img: 'https://images.unsplash.com/photo-1532578498858-e21a39e0a449?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-    correntAnswer: 'Germany'
+  question: 'Mercedes Benz',
+  questionOptions: ['Japan', 'United Kingdom', 'Germany', 'United States'],
+  img: 'https://images.unsplash.com/photo-1532578498858-e21a39e0a449?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+  correntAnswer: 'Germany'
 }, {
-    question: 'Ford',
-    questionOptions: ['Germany', 'Spain', 'United States', 'Italy'],
-    img: 'https://images.unsplash.com/photo-1530936541512-05ec5b6c6e71?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-    correntAnswer: 'United States'
+  question: 'Ford',
+  questionOptions: ['Germany', 'Spain', 'United States', 'Italy'],
+  img: 'https://images.unsplash.com/photo-1530936541512-05ec5b6c6e71?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+  correntAnswer: 'United States'
 }, {
-    question: 'BMW',
-    questionOptions: ['Germany', 'Italy', 'Denmark', 'United States'],
-    img: 'https://images.unsplash.com/photo-1552234816-0ea7b995a55c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1001&q=80',
-    correntAnswer: 'Germany'
+  question: 'BMW',
+  questionOptions: ['Germany', 'Italy', 'Denmark', 'United States'],
+  img: 'https://images.unsplash.com/photo-1552234816-0ea7b995a55c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1001&q=80',
+  correntAnswer: 'Germany'
 }, {
-    question: 'Cadillac',
-    questionOptions: ['Germany', 'Japan', 'United States', 'Denmark'],
-    img: 'https://images.unsplash.com/photo-1457088846009-9dbcf9d7643d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-    correntAnswer: 'United States'
+  question: 'Cadillac',
+  questionOptions: ['Germany', 'Japan', 'United States', 'Denmark'],
+  img: 'https://images.unsplash.com/photo-1457088846009-9dbcf9d7643d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+  correntAnswer: 'United States'
 }, {
-    question: 'Tesla',
-    questionOptions: ['Japan', 'United States', 'Spain', 'United Kingdom'],
-    img: 'https://images.unsplash.com/photo-1518194951576-93eec1215e9a?ixlib=rb-1.2.1&auto=format&fit=crop&w=1399&q=80',
-    correntAnswer: 'United States'
+  question: 'Tesla',
+  questionOptions: ['Japan', 'United States', 'Spain', 'United Kingdom'],
+  img: 'https://images.unsplash.com/photo-1518194951576-93eec1215e9a?ixlib=rb-1.2.1&auto=format&fit=crop&w=1399&q=80',
+  correntAnswer: 'United States'
 }]
 
 
